376ff100b34f7e3f1f908d09b8ea96b462f0a459
Eliton Penke <167805833+ElitonPenke@users.noreply.github.com>
2025-10-02 21:18:51 -0300

    Update Agenda.py

--- Agenda.py
+++ Agenda.py
@@ -1,43 +1,61 @@
+#importa a biblioteca json para manipular arquivo   
 import json
 
-ARQUIVO = 'contatos.json'
+#associa a variavel ARQUIVO ao arquivo json que vai se chamar contatos
+ARQUIVO = 'Minha agenda.json'
 
+#função para puxar o que tem dentro do arquivo json e tranformar em um dicionario py para manipular
 def carregar_contatos():
     try:
-        with open(ARQUIVO, 'r') as arquivo:
-            return json.load(arquivo)
+        with open(ARQUIVO, 'r') as Arquivo_temporario:
+            return json.load(Arquivo_temporario)
     except:
-        return []
+        return []  
 
-def salvar_contatos(contatos):
-    with open(ARQUIVO, 'w') as arquivo:
-        json.dump(contatos, arquivo, indent=4)
+#função para pegar a variavel em py e sobrescrever no arquivo JSON
 
+def salvar_contatos(contatos):   # PRECISA DE PARAMETRO PARA A FUNÇÃO SABER O QUE É PARA GUARDAR 
+    with open(ARQUIVO, 'w') as Arquivo_temporario:
+        json.dump(contatos, Arquivo_temporario, indent=4)
+
+#ao executar o programa a primeira coisa que faz é chamar a def carregar_contatos para dar o start no programa e
+#associa o return da def na variavel contatos(lista) 
 contatos = carregar_contatos()  
+#contatos é o banco de dados interno do programa
 
+
+#é feito uma class para ser feita de molde os cadasdtro no arquivo JSON
 class Agenda:
-    def adicionar_contato():
+
+#essa função vai adicionar novo contato na variavel py
+    def adicionar_contato(self):
         nome = input('NOME: ')
         telefone = input('TELEFONE: ')
         email = input('EMAIL: ')
         endereco = input('ENDEREÇO: ')
         aniversario = input('ANIVERSÁRIO: ')
-        id = len(contatos) + 1
+        id = len(contatos) + 1  #aqui é toda vez é criado uym novo id com base nos contatos ja salvos
+        
+        #uma variavel local para armazenar tudo de uma vez o que vai ser armazenado
         novo = {
             'id': id,
             'nome': nome.title(),
             'telefone': telefone,
-            'email': email.upper(),
+            'email': email.lower(),
             'endereco': endereco,
             'aniversario': aniversario
         }
+        #aqui ele vai adicionar a varivel (novo) a lista (contatos)
         contatos.append(novo)
+        
+        #aqui ele chama a def salvar_contatos para colocar a varial em py(a contatos) para dentro do arquivo JSON
         salvar_contatos(contatos)
         print('\nCONTATO ADICIONADO COM SUCESSO!\n')
 
-    def listar_contatos():
-        for contato in contatos:
-            print(f"\nID: {contato['id']}")
+    #def o qual vai retornar todos os contatos
+    def listar_contatos(self):
+        for contato in contatos:  # um laço de vai buscar cada contato na lista cointatos e vai trazer os itens abaixo
+            print(f"\nID: {contato['id']}")  #ele diz de onde é para buscar e o que
             print(f"NOME: {contato['nome']}")
             print(f"TELEFONE: {contato['telefone']}")
             print(f"EMAIL: {contato['email']}")
@@ -46,25 +64,44 @@ class Agenda:
             print("=========================================")
 
     def atualizar_contato(self):
-        pass
+        id = int(input("Me diga o 'ID' do contato que você quer atualizar: "))
+        dado_atualizado = input("O que você quer atualizar? (nome, telefone, email, endereco, aniversario): ").lower()
+        novo_valor = input(f"Qual o novo valor para {dado_atualizado}? ")
+        for contato in contatos:
+               contato[dado_atualizado] = novo_valor
+               salvar_contatos(contatos)  # ai ele chama a def para salvar a variavel em py para dentro do arquivo JSON
+               print(f"\nContato com ID {id} atualizado com sucesso!\n")
+               
+
     def deletar_contato(self):
-        pass
+        escolha_deletar = int(input("Me diga o 'ID' do contato que você quer deletar: "))
+        #Aqui ele faz uma nova lista com todos os contatos menos o que tem o id igual ao que o usuario escolheu, assim deletando o contato do arquivo
+        self.contatos = [contato for contato in self.contatos if contato['id'] != escolha_deletar]
+        salvar_contatos(self.contatos)  # ai ele chama a def para salvar a variavel em py para dentro do arquivo JSON
+        print(f"Contato com ID {escolha_deletar} deletado com sucesso!\n")
+
+#cria o objeto da class Agenda para chamar as funções as quais são estaticas
+agenda_principal = Agenda()
 
 
+#um loop infinito para fazaer de menu
 while True:
-    print('\n|--------1-CADASTRAR NOVO CONTATO---------|')
+    print('\n|--------1-CADASTRAR NOVO CONTATO-------|')
     print('|--------2-LISTAR MEUS CONTATOS-----------|')
     print('|--------3-EDITAR CONTATO-----------------|')
-    print('|--------4-SAIR-------------------------|\n')
+    print('|--------4-DELETAR CONTATO----------------|')
+    print('|--------5-SAIR---------------------------|\n')
     opcao=int(input('DIGITE A OPÇÃO DESEJADA: \n'))
 
     match opcao:
         case 1:
-            Agenda.adicionar_contato()
+            agenda_principal.adicionar_contato()
         case 2:
-            Agenda.listar_contatos()
+            agenda_principal.listar_contatos()
         case 3:
-            Agenda.atualizar_contato()
+            agenda_principal.atualizar_contato()
         case 4:
+            agenda_principal.deletar_contato()
+        case 5:
             print('SAINDO DO PROGRAMA...')
             break
